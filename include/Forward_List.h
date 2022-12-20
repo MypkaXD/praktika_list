ffbf8fbaaf1fdf2ea3a98d1d10562e51bcdc04dd
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-12-20 13:36:18 +0300

    Add files via upload

---
+++ include/Forward_List.h
@@ -0,0 +1,4 @@
+﻿#ifndef FORWARD_LIST_H
+#define FORWARD_LIST_H
+
+#endif // !FORWARD_LIST_H
---
+++ test/test_main.cpp
@@ -0,0 +1,12 @@
+#include <gtest.h>
+#include "Forward_List.h"
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+
+	system("pause");
+	std:: cout << "Для продолжения работы программы нажмите любую клавишу...";
+	std::cin.get();
+}
